ae76ed72d29af48a23908cf87c91dad99ddb24be
nichinichisou <nichinichisou0609@outlook.com>
2025-08-06 20:33:33 +0800

    feat: random mode piano and reverse don't apply at same time

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ except ImportError:
 PLUGIN_NAME = "pjsk_guess_song"
 PLUGIN_AUTHOR = "nichinichisou"
 PLUGIN_DESCRIPTION = "PJSK猜歌插件"
-PLUGIN_VERSION = "1.1.0"
+PLUGIN_VERSION = "1.1.1"
 PLUGIN_REPO_URL = "https://github.com/nichinichisou0609/astrbot_plugin_pjsk_guess_song"
 
 
